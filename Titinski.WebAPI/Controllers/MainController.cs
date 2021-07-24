20cd2041b3d79c57c4ef5b3fcb6866666f71d535
yoadwo <yoadw20@gmail.com>
2021-07-24 22:36:45 +0300

    add: empty get endpoint

---
+++ Titinski.WebAPI/Controllers/MainController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Titinski.WebAPI.Handlers;
+
+namespace Titinski.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MainController : ControllerBase
+    {
+        private readonly ILogger<MainController> _logger;
+
+        private IMainHandler _mainHandler;
+
+        public MainController(
+            IMainHandler mainHandler,
+            ILogger<MainController> logger
+            )
+        {
+            _mainHandler = mainHandler;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("rant")]
+        public IActionResult GetRant()
+        {
+            return _mainHandler.GetRantAsync();
+        }
+
+    }
+}
---
+++ Titinski.WebAPI/Handlers/MainHandler.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Titinski.WebAPI.Handlers
+{
+    public class MainHandler : IMainHandler
+    {
+        private readonly ILogger<MainHandler> _logger;
+        private readonly Random _rnd;
+
+        public MainHandler(
+            ILogger<MainHandler> logger
+            )
+        {
+            _logger = logger;
+            _rnd = new Random();
+        }
+
+        public IActionResult GetRantAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
