9e4b823a54b4b5fc96e31daac5de409cd9e99658
yoadwo <yoadw20@gmail.com>
2021-08-05 16:20:16 +0300

    fix: file not saved on ftp (incorrect url)

--- Titinski.WebAPI/Services/ImageRepository/FtpRepo.cs
+++ Titinski.WebAPI/Services/ImageRepository/FtpRepo.cs
@@ -11,7 +11,7 @@ namespace Titinski.WebAPI.Services.ImageRepository
 {
     public class FtpRepo : IImageRepo
     {
-        private const string FTP_IMAGES_DIR = "//home/u520350406/domains/wolfsthal.org/trt1/the_real_titinski/uploaded/images";
+        private const string FTP_IMAGES_DIR = "/the_real_titinski/uploaded/images";
         private readonly IOptions<AppSettings.FtpConfig> _ftpConfig;
 
         public FtpRepo(
@@ -24,7 +24,7 @@ namespace Titinski.WebAPI.Services.ImageRepository
         public void AddRant(RantPost rant)
         {
             // Get the object used to communicate with the server.
-            FtpWebRequest request = (FtpWebRequest)WebRequest.Create(_ftpConfig.Value.Address + FTP_IMAGES_DIR);
+            FtpWebRequest request = (FtpWebRequest)WebRequest.Create(_ftpConfig.Value.Address + FTP_IMAGES_DIR + $"/{rant.ImageFile.Name}_{DateTime.Now.ToString("s")}");
             request.Method = WebRequestMethods.Ftp.UploadFile;
 
             // This example assumes the FTP site uses anonymous logon.
@@ -32,22 +32,22 @@ namespace Titinski.WebAPI.Services.ImageRepository
 
             // Copy the contents of the file to the request stream.
             byte[] fileContents;
-            using (var ms = new System.IO.MemoryStream())
-            {
-                rant.ImageFile.CopyTo(ms);
-                fileContents = ms.ToArray();
-            }
+            
+            request.ContentLength = rant.ImageFile.Length;
 
-            request.UseBinary = true;
-            request.ContentLength = fileContents.Length;
 
             using (Stream requestStream = request.GetRequestStream())
             {
-                requestStream.Write(fileContents, 0, fileContents.Length);
+                //requestStream.Write(fileContents, 0, fileContents.Length);
+                rant.ImageFile.CopyTo(requestStream);
             }
 
             using (FtpWebResponse response = (FtpWebResponse)request.GetResponse())
             {
+                Stream responseStream = response.GetResponseStream();
+                StreamReader reader = new StreamReader(responseStream);
+                Console.WriteLine(reader.ReadToEnd());
+
                 Console.WriteLine($"Upload File Complete, status {response.StatusDescription}");
             }
         }
