d1573eae3055f9aab98eb1ed1aff64a1f90a4333
yoadwo <yoadw20@gmail.com>
2021-10-21 15:29:22 +0300

    add: top card

--- Titinski.Client/src/app/app.module.ts
+++ Titinski.Client/src/app/app.module.ts
@@ -8,13 +8,11 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 import { AppComponent } from './app.component';
-import { StickyHeaderComponent } from './components/sticky-header/sticky-header.component';
 import { RantDetailComponent } from './components/rant-detail/rant-detail/rant-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    StickyHeaderComponent,
     RantDetailComponent
   ],
   imports: [
